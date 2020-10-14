b820ed37f03da7020aaa3ef6a9245ffcdb409597
Pranali Bagade <bagadepranali60@gmail.com>
2020-10-14 18:07:06 +0530

    pranali java

---
+++ Ctsjp.java
@@ -0,0 +1,7 @@
+public class Ctsjp
+{ 
+             public static void main (String [] args)
+                {
+                        System.out.println("Welcome to octomber");
+                }
+}
---
+++ Ctsjppranali.java
@@ -0,0 +1,7 @@
+public class Ctsjppranali
+{ 
+             public static void main (String [] args)
+                {
+                        System.out.println("ALL THE BEST");
+                }
+}
